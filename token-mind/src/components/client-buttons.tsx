846ab34ca778ba3d4622bc18f83a09e2f6561b66
Timothy <knbtimothy@gmail.com>
2025-04-15 15:04:30 +0100

    .

--- token-mind/src/components/client-buttons.tsx
+++ token-mind/src/components/client-buttons.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useUser } from "@civic/auth-web3/react";
 import { Button } from "./ui/button";
-import { useRouter } from "next/navigation";
 
 export function GetStartedButton({ children, text } : {
     children?: React.ReactNode,
