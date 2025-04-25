1c4dc77834a48ea11c22fdee008dc9afffbf46a7
Timothy <knbtimothy@gmail.com>
2025-04-25 12:04:50 +0100

    fixed unused variable error

--- token-mind/src/components/client-buttons.tsx
+++ token-mind/src/components/client-buttons.tsx
@@ -2,18 +2,16 @@
 import { useUser } from "@civic/auth-web3/react";
 import { Button } from "./ui/button";
 import { toast } from "sonner";
-import { useRouter } from "next/navigation";
 
 export function GetStartedButton({ children, text } : {
     children?: React.ReactNode,
     text: string
 }) {
   const { signIn } = useUser();
-  const navigate = useRouter();
+
   const handleSignIn = async () => {
     try {
       await signIn();
-      toast('Sign in successful');
     } catch (error) {
       console.log('error:',error)
     }
